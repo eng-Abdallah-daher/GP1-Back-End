fbe85667cfcf844ab34e3bd9e1cd8948fa31b023
eng-Abdallah-daher <abdallahdaher205@gmail.com>
2024-12-09 18:05:41 +0200

    Update towingServiceController.js

--- controllers/towingServiceController.js
+++ controllers/towingServiceController.js
@@ -1,4 +1,4 @@
-const TowingService = require('../models/towingserviceModel');
+const TowingService = require('../models/towingServiceController');
 
 const TowingServiceController = {
     create: async (req, res) => {
