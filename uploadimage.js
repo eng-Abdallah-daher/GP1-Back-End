d80f0679bc8ae5bc63b41dcabe6316acc8b72134
AbdallahDaher <abdallahdaher785@gmail.com>
2025-01-25 00:15:29 +0200

    back

--- uploadimage.js
+++ uploadimage.js
@@ -1,39 +1,33 @@
 const axios = require("axios");
 const FormData = require("form-data");
-const path = require("path");
-const fs = require("fs");
+
 async function uploadImage(base64Data) {
-    let y;
+  let result;
 
-    try {
-        const buffer = Buffer.from(base64Data.split(',')[1], 'base64');
-        const tempPath = path.join(__dirname, 'temp_image.png');
-        fs.writeFileSync(tempPath, buffer);
+  try {
+    const buffer = Buffer.from(base64Data.split(",")[1], "base64");
 
-        const formData = new FormData();
-        formData.append('image', fs.createReadStream(tempPath));
+    const formData = new FormData();
+    formData.append("image", buffer, "image.png");
 
-        const headers = {
-            'Authorization': 'Client-ID bae1c7177f1cb8d',
-            ...formData.getHeaders(),
-        };
+    const headers = {
+      Authorization: "Client-ID bae1c7177f1cb8d",
+      ...formData.getHeaders(),
+    };
 
-        const uploadResult = await axios.post('https://api.imgur.com/3/upload', formData, { headers });
+    const uploadResult = await axios.post(
+      "https://api.imgur.com/3/upload",
+      formData,
+      { headers }
+    );
 
-        console.log('Upload Result:', uploadResult.data);
+    result = uploadResult.data;
+  } catch (error) {
+    console.error("Error uploading image:", error.message);
+    throw error;
+  }
 
-        const imageUrl = uploadResult.data.data.link;
-
-        console.log('Image URL:', imageUrl);
-        y = imageUrl;
-
-        fs.unlinkSync(tempPath);
-
-    } catch (error) {
-        console.error('Error uploading image:', error);
-    }
-
-    return y;
+  return result["data"]["link"];
 }
 
 module.exports = uploadImage;
