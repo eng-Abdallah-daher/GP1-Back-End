af0aec47e21216d0574f7a3bc1527a646ae56670
AbdallahDaher <abdallahdaher785@gmail.com>
2025-01-25 00:56:10 +0200

    w

--- uploadimage.js
+++ uploadimage.js
@@ -1,43 +1,43 @@
-const axios = require("axios");
+const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 const path = require("path");
 
 async function uploadImage(base64Data) {
   let y;
-  const CLIENT_ID = "bae1c7177f1cb8d";
+  cloudinary.config({
+    cloud_name: "dkass9jnq",
+    api_key: "936456332441919",
+    api_secret: "xzgm6MxZXjwS_n1bzjGquAO7tsQ",
+  });
 
   try {
     const buffer = Buffer.from(base64Data.split(",")[1], "base64");
     const tempPath = path.join(__dirname, "temp_image.png");
     fs.writeFileSync(tempPath, buffer);
 
-    const imageData = fs.readFileSync(tempPath, { encoding: "base64" });
+    const uploadResult = await cloudinary.uploader.upload(tempPath, {
+      public_id: path.basename(tempPath, path.extname(tempPath)),
+    });
 
-    const response = await axios.post(
-      "https://api.imgur.com/3/upload",
-      {
-        image: imageData,
-        type: "base64",
-      },
-      {
-        headers: {
-          Authorization: `Client-ID ${CLIENT_ID}`,
-          "Content-Type": "application/json",
-        },
-        maxContentLength: 50 * 1024 * 1024, 
-        maxBodyLength: 50 * 1024 * 1024, 
-      }
-    );
+    console.log("Upload Result:", uploadResult);
 
-    const uploadResult = response.data.data;
-    const imageUrl = uploadResult.link;
-    y = imageUrl;
-
-    const optimizedUrl = `${imageUrl}?auto=compress`;
-    const autoCropUrl = `${imageUrl}?crop=auto&gravity=auto&width=500&height=500`;
+    const imageUrl = uploadResult.secure_url;
 
     console.log("Image URL:", imageUrl);
+    y = imageUrl;
+    const optimizedUrl = cloudinary.url(uploadResult.public_id, {
+      fetch_format: "auto",
+      quality: "auto",
+    });
+
     console.log("Optimized Image URL:", optimizedUrl);
+
+    const autoCropUrl = cloudinary.url(uploadResult.public_id, {
+      crop: "auto",
+      gravity: "auto",
+      width: 500,
+      height: 500,
+    });
     console.log("Auto-cropped Image URL:", autoCropUrl);
 
     fs.unlinkSync(tempPath);
